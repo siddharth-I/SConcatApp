1ce3581d5ceedbb629de967355cecadfd2ca897e
Siddharth Malewar <sidmalewar57@gmail.com>
2021-07-26 10:42:44 -0400

    initial commit

---
+++ SConcatApp/Models/ConcatModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SConcatApp.Models
+{
+    public class ConcatModel
+    {
+        public string Word { get; set; }
+
+        public int Repetitions { get; set; }
+    }
+}
